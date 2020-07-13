b282caf93a4b364048bbde60e4a309a8bae26275
shonburgundy <seanbeloussow@yahoo.com>
2020-07-13 15:07:30 -0700

    should done this awhile ago

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Sidebar = () => (
+  <div className='sidebar' data-testid="sidebar">
+    <ul className='sidebar_generic'>
+      <li>Inbox</li>
+      <li>Today</li>
+      <li>Next 7 days</li>
+    </ul>
+  </div>
+);
+
+b = block
+e = element
+m = modifier
